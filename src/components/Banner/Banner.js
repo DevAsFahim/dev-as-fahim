a930358c7b011366366288280f6cbe85ede93ad3
ASFahim007 <devasfahim@gmail.com>
2022-12-11 20:15:38 +0600

    portfolio button and others

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import profile from '../../assets/images/profile.png'
+import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className='banner container m-auto h-screen flex justify-center flex-col'>
+        <div className='banner relative w-full lg:w-[1070px] m-auto h-screen flex justify-center flex-col'>
             <p className='text-warning text-2xl font-bold mb-4'>Hi there, I am</p>
             <h1 className="md:text-7xl text-4xl font-bold text-slate-50 mb-5">A S Fahim.</h1>
             <h1 className="md:text-7xl text-4xl font-bold text-slate-500 mb-6">MERN Stack Developer.</h1>
             <p className="text-slate-500 md:w-1/2">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product. I have good hand on backend also.</p>
+            <img src={profile} alt="" />
         </div>
     );
 };
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,33 +7,39 @@ import { Link } from 'react-router-dom';
 
 const Projects = () => {
     return (
-        <div id='works' className='project container m-auto'>
+        <div id='works' className='project w-full lg:w-[1070px] m-auto'>
             <div className="text-center mb-10 w-1/2 m-auto">
                 <h4 className="text-5xl font-bold mb-4">Some Things I've Built</h4>
                 <p>Here you will find some of the personal and clients projects that I created with each project containing its own case study</p>
             </div>
             <div className="project_cards grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <a href='https://auto-plus-fc1d8.web.app/'>
-                    <div className="project_cart">
-                        <img src={auto} alt="" />
-                        <h3 className="text-3xl font-bold text-slate-100">Auto Plus</h3>
-                        <p className="text-2xl">Car reselling website</p>
+                <div className="project_cart">
+                    <img src={auto} alt="" />
+                    <h3 className="text-3xl font-bold text-slate-100">Auto Plus</h3>
+                    <p className="text-2xl">Car reselling website</p>
+                    <div className="project_btn">
+                        <a target='_blank' rel='noreferrer' href="https://auto-plus-fc1d8.web.app/"><button>View Project</button></a>
+                        <button>View Details</button>
                     </div>
-                </a>
-                <a href="https://meta-zone.web.app/">
-                    <div className="project_cart">
-                        <img src={meta} alt="" />
-                        <h3 className="text-3xl font-bold text-slate-100">Auto Plus</h3>
-                        <p className="text-2xl">Car reselling website</p>
+                </div>
+                <div className="project_cart">
+                    <img src={meta} alt="" />
+                    <h3 className="text-3xl font-bold text-slate-100">Meta Zone</h3>
+                    <p className="text-2xl">Photographer Portfolio</p>
+                    <div className="project_btn">
+                        <a target='_blank' rel='noreferrer' href="https://meta-zone.web.app/"><button>View Project</button></a>
+                        <button>View Details</button>
                     </div>
-                </a>
-                <a href="https://e-academy-e31d2.web.app/">
-                    <div className="project_cart">
-                        <img src={academy} alt="" />
-                        <h3 className="text-3xl font-bold text-slate-100">Auto Plus</h3>
-                        <p className="text-2xl">Car reselling website</p>
+                </div>
+                <div className="project_cart">
+                    <img src={academy} alt="" />
+                    <h3 className="text-3xl font-bold text-slate-100">EAcademy</h3>
+                    <p className="text-2xl">Online Academy</p>
+                    <div className="project_btn">
+                        <a target='_blank' rel='noreferrer' href="https://e-academy-e31d2.web.app/"><button>View Project</button></a>
+                        <button>View Details</button>
                     </div>
-                </a>
+                </div>
             </div>
         </div>
     );
